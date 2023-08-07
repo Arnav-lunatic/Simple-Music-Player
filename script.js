d44018c4ccf24ec5218ed8358ce50216bac29ef3
arnav <arnav4c@gmail.com>
2023-08-07 22:48:34 +0530

    update css

--- script.js
+++ script.js
@@ -68,12 +68,14 @@ function PlayPauseSong(){
 
         playPauseButton.innerHTML = `<svg class='pause' id="controlButton" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><style>svg{fill:#ffffff}</style><path d="M48 64C21.5 64 0 85.5 0 112V400c0 26.5 21.5 48 48 48H80c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm192 0c-26.5 0-48 21.5-48 48V400c0 26.5 21.5 48 48 48h32c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H240z"/></svg>`
         cover.style.animationName = 'rotateImg'
+        cover.style.border = '1px solid rgba(255, 255, 255, 0.233)'
         cdDesign.style.opacity = '1'
     }
     else if(pause===1){
         audio.pause()
         pause=0
         playPauseButton.innerHTML = `<svg class="play" id="controlButton" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><style>svg{fill:#ffffff}</style><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>`
+        cover.style.border = ''
         cover.style.animationName = ''
         cdDesign.style.opacity = '0'
     }
